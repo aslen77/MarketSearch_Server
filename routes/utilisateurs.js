a6873b37f7f53f5455d111902421d2c3a8c395bd
Guesmi Aslen <aslen9@hotmail.fr>
2022-08-26 18:19:58 +0200

    add validation post user

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -1,11 +1,32 @@
 const { application } = require("express");
 const express = require("express")
-
+const {check, validationResult} = require('express-validator')
 const router = express.Router(); 
 
 const Utilisateur = require('../models/Utilisateur')
 
+const validate = [
+  check('nom')
+  .isLength({min : 3 , max : 10})
+  .withMessage('le nom doit contenir minimum 3 caractères et 10 caractères ! '),
+  check('prenom')
+  .isLength({min : 3 , max : 10})
+  .withMessage('le prenom doit contenir minimum 3 caractères et 10 caractères ! '),
+  check('mdp')
+  .isLength({min : 3 , max : 20})
+  .withMessage('le mot de passe doit contenir minimum 8 caractères ! '),
+  check('email')
+  .isEmail().withMessage('email invalide')
+  .isLength({min : 5 })
+  .withMessage('le mot de passe doit contenir minimum 5 caractères ! '),
+  check('tel')
+  .isNumeric().withMessage('doit avoir que des chiffres')
+  .isLength({min : 8 , max : 8})
+  .withMessage('le tel doit contenir 8 chiffres ! ')
 
+
+
+]
 //api/utilisateurs
 // SELECT GET ALL Users
 router.get("/", (req, res) => {
@@ -29,8 +50,15 @@ router.get("/:_id", (req, res) => {
 // api/utilisateurs
 //POST
 
-router.post('/', (req,res)=> {
+router.post('/',validate , (req,res)=> {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
     const utilisateur = new Utilisateur({
+
         nom : req.body.nom,
         prenom : req.body.prenom,
         email : req.body.email,
