51d6fd38b6a39f4d3519fc31e0a3ea7927afd05f
Guesmi Aslen <aslen9@hotmail.fr>
2022-10-20 11:48:20 +0200

    config index

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const cors = require('cors')
 const path = require('path')
-
-require('dotenv').config();
-
+require('dotenv').config()
 const utilisateurs = require('./routes/utilisateurs')
 const vendeurs = require('./routes/vendeurs')
 const produits = require('./routes/produits')
 const administrateurs = require('./routes/administrateurs')
 const categories = require('./routes/categories')
 const scategories = require('./routes/sous_categories')
-const favoris = require('./routes/favoris')
+const favoris = require('./routes/Favoris')
+// const upload = require("./routes/upload")
 const upload = require("./routes/images")
-
-app.use(cors());
 app.use(express.json())
 
 app.use(express.static('uploads'));
+
+// app.use('/api/images', express.static(path.join(__dirname,"uplaods")));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the app marketSearch')
 })
 
+    
+          
+            
+    
+
+          
+    
+    
+  
 app.use('/api/utilisateurs', utilisateurs)
 app.use('/api/produits', produits)
 app.use('/api/admins', administrateurs)
@@ -32,10 +39,9 @@ app.use('/api/sous_categories', scategories)
 app.use('/api/favoris', favoris)
 app.use('/api/vendeurs', vendeurs)
 app.use('/api', upload)
-const port = process.env.PORT || 3000
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.b38myqm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+mongoose.connect('mongodb+srv://Arfaoui_Chayma:adminadmin@cluster0.b38myqm.mongodb.net/MarketSearch?retryWrites=true&w=majority')
     .then(result => {
         app.listen(port, () => console.log(`Server is running on port ${port}`))
     })
     .catch(err => console.log('err'))
-
+const port = process.env.PORT || 3000
